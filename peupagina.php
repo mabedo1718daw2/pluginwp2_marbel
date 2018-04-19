7efaf1be16c7bcc8900986e7f84f37726379f7ab
mabedo1718daw2 <mabedo1718daw2@gmail.com>
2018-04-19 23:40:32 +0200

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/mabedo1718daw2/pluginwp2_marbel.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Marc Bellido
+Author URI: https://github.com/mabedo1718daw2
+License: GNU GPLv3
+*/
+?>
