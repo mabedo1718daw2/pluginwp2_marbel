6b2fda9a5d289de7e576af59d0fa2fa860cb14b3
mabedo1718daw2 <38224302+mabedo1718daw2@users.noreply.github.com>
2018-04-23 21:14:22 +0200

    Update peupagina.php
    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -8,4 +8,12 @@ Author: Marc Bellido
 Author URI: https://github.com/mabedo1718daw2
 License: GNU GPLv3
 */
+function admin_menu_peu_pagina(){
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
+	$role="administrador";
+	$id="peu_pagina_per_defecte";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id);
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
