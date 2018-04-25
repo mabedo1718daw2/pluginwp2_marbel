1e1e62ed1b9f7c78bf6a319938d8f8d376658414
mabedo1718daw2 <38224302+mabedo1718daw2@users.noreply.github.com>
2018-04-25 17:29:53 +0200

    commit3

--- peupagina.php
+++ peupagina.php
@@ -11,18 +11,19 @@ License: GNU GPLv3
 function admin_menu_peu_pagina(){
         $titolPagina="Peu de pàgina per defecte";
         $titolEntrada="Peu de pàgina per defecte";
-        $role="administrador";
+        $role="administrator";
         $id="peu_pagina_per_defecte";
-        add_menu_page($titolPagina,$titolEntrada,$role,$id);
+        $nom_funcio="nouPeuPagDef";
+        add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio);
 }
 add_action('admin_menu','admin_menu_peu_pagina');
+
 function nouPeuPagDef(){
         include_once('formPeuPag.php');
 }
-function afegPeuPagDef(){
-    echo "<div style='color: red;
-    font-size: 10px;
-    margin: 10px;'>".get_option('footer_text')."</div>";
-}
+
 add_action('wp_footer','afegPeuPagDef');
+function afegPeuPagDef(){
+        echo "<div style='color: red; font-size: 10px; margin: 10px;'>".get_option('footer_text')."</div>";
+}
 ?>
